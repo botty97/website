1c85faf30a24be23fa392fd4e2646382bb1794b0
Connor Botterill <connorbotterill@Connors-MacBook-Pro.local>
2021-03-16 22:55:03 +0000

    First Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = 3112;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/about.html"));
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/contact.html"));
+});
+
+app.get("/post", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/post.html"));
+});
+
+app.listen(port, () => {
+  console.log(`App is listening on ${port}`);
+});
